bd1edb2b5a254b73d94aa9ff8874dd9a17f4dbb2
Gekif <dzulfikar.maulana@gmail.com>
2025-05-20 17:23:39 +0700

    Latihan Modul-2

---
+++ Modul-2/latihan.py
@@ -0,0 +1,19 @@
+# Buat variabel panjang = 10 dan lebar = 5, lalu hitung luas persegi panjang.
+
+panjang = 10 
+lebar = 5
+
+print("Panjang: ", panjang)
+print("Lebar: ", lebar)
+print("Luas: ", panjang * lebar)
+
+
+# Tampilkan hasil dari "Python " * 3 => False
+print("Python " * 3)
+
+
+# Ubah nilai "3.14" menjadi tipe float lalu kalikan dengan 2.
+# print(float("3.14") * 2)
+nilai = float("3.14")
+hasil = nilai * 2
+print(hasil)
