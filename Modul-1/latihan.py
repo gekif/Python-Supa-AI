1951f91da8540f47cc6d0055276636a963f1e82b
Gekif <dzulfikar.maulana@gmail.com>
2025-05-20 12:43:54 +0700

    latihan module 1

---
+++ Modul-1/latihan.py
@@ -0,0 +1,12 @@
+# Tulis program Python yang menampilkan kalimat: Selamat belajar Python!
+print("Selamat belajar Python!")
+
+# Tulis program Python yang menampilkan dua baris teks berikut secara berurutan:
+print("Baris pertama")
+print("Baris kedua")
+
+# Tulis program Python yang menampilkan kalimat "Halo Dunia!" sebanyak 3 kali menggunakan perintah print()
+print("Halo Dunia!")
+print("Halo Dunia!")    
+print("Halo Dunia!") 
+
