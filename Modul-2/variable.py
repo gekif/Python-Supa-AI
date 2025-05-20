ded7fe55b901cdb14f2d134832320663c6169cfb
Gekif <dzulfikar.maulana@gmail.com>
2025-05-20 17:17:22 +0700

    Modul-2

---
+++ Modul-2/variable.py
@@ -0,0 +1,22 @@
+nama = "Andi"
+usia = 20 
+
+print("Nama saya adalah " + nama)
+print("Usia saya adalah " + str(usia))
+
+
+#  Operasi Aritmatika
+print(5 + 3) # Penjumlahan
+print(5 - 3) # Pengurangan
+
+
+# Manipulasi String
+print("Hello " + "World") # Menggabungkan String => Hello World
+
+
+# Logika
+print(True and False) # False
+
+
+# Konversi Tipe Data
+print(int("123") + 456) # 579
