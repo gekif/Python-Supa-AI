f8fa1d1f592d10c1cb48bb5eca93b566ca89307c
Gekif <dzulfikar.maulana@gmail.com>
2025-05-20 17:47:58 +0700

    Latihan Modul-4

---
+++ Modul-4/latihan.py
@@ -0,0 +1,20 @@
+# Buat fungsi kuadrat yang menerima satu parameter dan mengembalikan nilai kuadrat dari parameter tersebut
+
+def kuadrat(x):
+    return x ** 2
+
+print(kuadrat(5))  # Output: 25
+
+
+# Buat list berisi nama-nama hewan, lalu tambahkan satu hewan baru ke list tersebut.
+
+hewan = ["kucing", "anjing", "burung"]
+hewan.append("ikan")
+print(hewan)  # Output: ['kucing', 'anjing', 'burung', 'ikan']
+
+
+# Buat tuple berisi tiga angka, lalu tampilkan angka kedua dari tuple tersebut
+
+angka_tuple = (10, 20, 30)
+
+print(angka_tuple[1])  # Output: 20
