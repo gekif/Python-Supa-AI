78e9d4e2f789db9581095d57fd09f4ad7616499c
Gekif <dzulfikar.maulana@gmail.com>
2025-05-20 17:39:11 +0700

    Latihan Modul-3

---
+++ Modul-3/latihan.py
@@ -0,0 +1,23 @@
+# Buat program untuk memeriksa apakah sebuah angka lebih besar dari 10. Jika iya, tampilkan "Lebih besar dari 10", jika tidak, tampilkan "10 atau lebih kecil"
+
+angka = 15
+
+if angka > 10:
+    print("Lebih besar dari 10")
+else:
+    print("10 atau lebih kecil")
+
+
+# Tampilkan angka 1 hingga 5 menggunakan loop for
+
+for i in range(1, 6):
+    print(i)
+
+
+# Tampilkan angka 5 hingga 1 menggunakan loop while
+
+hitung = 5
+
+while hitung > 0:
+    print(hitung)
+    hitung -= 1
