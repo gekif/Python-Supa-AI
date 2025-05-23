adaa587fc681aecf1617ed566329405dc73582d4
Gekif <dzulfikar.maulana@gmail.com>
2025-05-23 16:11:59 +0700

    Modul-10: Koleksi - List

---
+++ Modul-10/koleksi-list.py
@@ -0,0 +1,14 @@
+# Membuat list
+buah = ["apel", "jeruk", "pisang"]
+
+# Menambahkan elemen
+buah.append("mangga")
+
+# Mengakses elemen
+print(buah[1]) # Output: jeruk
+
+# Mengubah elemen
+buah[0] = "Stroberi"
+
+# Menampilkan seluruh list
+print(buah)
