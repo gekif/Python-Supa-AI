79ec90478185ef37b0c0c30deed110224898bc02
Gekif <dzulfikar.maulana@gmail.com>
2025-05-23 16:21:55 +0700

    Modul-10: Koleksi - Latihan

---
+++ Modul-10/latihan.py
@@ -0,0 +1,22 @@
+# List
+daftar_buah = ["apel", "jeruk", "pisang"]
+
+# Tuple
+koordinat = (10, 20)
+
+# Set
+genre_buku = {"Fiksi", "Non-Fiksi", "Teknologi"}
+
+# Dictionary
+buku = {
+    "judul": "Belajar Python",
+    "penulis": "John Doe",
+    "tahun_terbit": 2023
+}
+
+
+# Menampilkan data
+print("Daftar Buah:", daftar_buah)
+print("Koordinas:", koordinat)
+print("Genre Buku:", genre_buku)
+print("Data Buku:", buku)
