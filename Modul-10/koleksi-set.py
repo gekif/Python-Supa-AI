ba779b83c74861979ee83a26f279b26b5a3146ce
Gekif <dzulfikar.maulana@gmail.com>
2025-05-23 16:16:30 +0700

    Modul-10: Koleksi - Set

---
+++ Modul-10/koleksi-set.py
@@ -0,0 +1,13 @@
+# Membuat set
+angka = {1, 2, 3, 3, 4}
+
+# Menampilkan set (duplikat otomatis dihapus)
+print(angka) # Output {1, 2, 3, 4}
+
+# Menambahkan elemen
+angka.add(5)
+
+# Menghapus elemen
+angka.remove(2)
+
+print(angka)
