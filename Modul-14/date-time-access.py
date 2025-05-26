e99c68bba044414efd8ddcc2743d1ee842c92504
Gekif <dzulfikar.maulana@gmail.com>
2025-05-26 17:40:09 +0700

    Modul 14: Datetime - Akses komponen tanggal & waktu

---
+++ Modul-14/date-time-access.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+now = datetime.now()
+
+print("Tahun:", now.year)
+print("Bulan:", now.month)
+print("Hari:", now.day)
+print("Jam:", now.hour)
+print("Menit:", now.minute)
+print("Detik:", now.second)
